ac376dfa3da3d4e309bcd093daa2d982efbae127
soichi777 <soujiro0725@gmail.com>
2015-04-12 14:21:52 +0900

    simple command interface added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
+/**
+ * TODO namespace
+ */
 #include <iostream>
 #include "../include/WirelessCommunication/XBeeAPIFrameBuilder.h"
 
 using namespace std;
 
+enum Command {Right, Left, Proceed, Run, Invalid};
+
+void right()
+{
+  cout << "rotating clockwisely\n";
+}
+
+void left()
+{
+  cout << "rotating counter-clockwisely\n";
+}
+
+void proceed()
+{
+  cout << "moving forward\n";
+}
+
+void run()
+{
+  cout << "executing a sequence of commands\n";
+}
+
+Command select()
+{
+  int tmp;
+  do {
+    cout << "(0)right, (1)left, (2)proceed, (3)run, (4)end\n";
+    cin >> tmp;
+  } while (tmp < Right || tmp > Invalid);
+  return static_cast<Command>(tmp);
+}
+
 int main()
 {
-  XBeeAPIFrameBuilder* builder;
-  builder = new XBeeAPIFrameBuilder();
+  typedef void (*CommandFP)();
+  Command selected;
+  CommandFP apf[] = {right, left, proceed, run};
 
-  //char command[] = "run";
-  uint8_t *command;
+  do {
+    selected = select();
+    if (selected >= Right && selected < Invalid) {
+      apf[selected]();
+    }
+  } while (selected != Invalid);
   
-  cout << "Hello World" << endl;
-  cout << builder->build(command).GetFrameData() << endl;
+  // XBeeAPIFrameBuilder* builder;
+  // builder = new XBeeAPIFrameBuilder();
+  // cout << builder->build(command).GetFrameData() << endl;
   
   return 0;
 }
